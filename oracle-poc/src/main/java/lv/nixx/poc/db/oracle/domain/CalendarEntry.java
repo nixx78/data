8d5491fc9e4066d038e8f7c7526c2e92100bbe0d
nixx78 <nixx78@gmail.com>
2017-09-20 09:22:44 +0300

    Initial project commit

---
+++ oracle-poc/src/main/java/lv/nixx/poc/db/oracle/domain/CalendarEntry.java
@@ -0,0 +1,8 @@
+package lv.nixx.poc.db.oracle.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class CalendarEntry {
+
+}
---
+++ oracle-poc/src/test/java/lv/nixx/poc/db/oracle/SimpleSpCall.java
@@ -0,0 +1,5 @@
+package lv.nixx.poc.db.oracle;
+
+public class SimpleSpCall {
+ //https://stackoverflow.com/questions/14900708/java-calling-a-stored-procedure-in-an-oracle-database
+}
