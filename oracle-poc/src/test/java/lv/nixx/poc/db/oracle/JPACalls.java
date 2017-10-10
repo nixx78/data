016a60fd1de2a57f63ce6ca685d8c538db61e795
nixx78 <nixx78@gmail.com>
2017-10-10 22:04:32 +0300

    Sample how to call SP using createStoredProcedureQuery added

--- oracle-poc/src/test/java/lv/nixx/poc/db/oracle/JPACalls.java
+++ oracle-poc/src/test/java/lv/nixx/poc/db/oracle/JPACalls.java
@@ -4,14 +4,18 @@ import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.ParameterMode;
 import javax.persistence.Persistence;
 import javax.persistence.StoredProcedureQuery;
 
 import org.junit.Test;
+
+import lv.nixx.poc.db.oracle.domain.DayStatistic;
 
 public class JPACalls {
 	
@@ -41,6 +45,24 @@ public class JPACalls {
                 
         System.out.println(count);
 		
+	}
+	
+	
+	@Test
+	public void createProcedureCallRuntime() throws ParseException {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("oracle-poc");
+        EntityManager em = factory.createEntityManager();
+
+        StoredProcedureQuery query = em.createStoredProcedureQuery("sandbox_sp.get_bugs_by_period", DayStatistic.class);
+        query.registerStoredProcedureParameter("start_date", Date.class, ParameterMode.IN);
+        query.registerStoredProcedureParameter("end_date", Date.class, ParameterMode.IN);
+        query.registerStoredProcedureParameter("bugs", DayStatistic.class, ParameterMode.REF_CURSOR);
+        
+        query.setParameter("start_date", sdf.parse("01.09.2017"));
+        query.setParameter("end_date", sdf.parse("10.09.2017"));
+        
+        List<?> resultList = query.getResultList();
+        resultList.forEach(System.out::println);
 	}
 
 }
