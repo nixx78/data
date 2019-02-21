305c7d1798d34b096748a3a41d0a4db270ef71a7
nixx78 <nixx78@gmail.com>
2019-02-21 09:25:04 +0200

    More sample added

--- SpringDataSample/src/main/java/lv/nixx/poc/spring/data/repository/txn/TransactionRepository.java
+++ SpringDataSample/src/main/java/lv/nixx/poc/spring/data/repository/txn/TransactionRepository.java
@@ -1,7 +1,11 @@
 package lv.nixx.poc.spring.data.repository.txn;
 
 import java.util.Collection;
+import java.util.stream.Stream;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import lv.nixx.poc.spring.data.domain.txn.Currency;
@@ -12,5 +16,10 @@ public interface TransactionRepository extends PagingAndSortingRepository<Transa
 	public Collection<Transaction> findAllByCurrency(Currency currency);
 	public Collection<Transaction> findAllByAccountOrderByAmount(String account);
 	public Collection<Transaction> findAllByCurrencyAlphaCode(String alphaCode);
-
+	public Collection<Transaction> findTop3ByOrderByAmountDesc();
+	public Collection<Transaction> findFirst3ByCurrencyAlphaCode(String alphaCode, Sort sort);
+	
+	@Query("select t from Transaction t")
+	public Stream<Transaction> streamAllPaged(Pageable pageable);
+	
 }
--- SpringDataSample/src/test/java/lv/nixx/poc/spring/data/repository/txn/TransactionTest.java
+++ SpringDataSample/src/test/java/lv/nixx/poc/spring/data/repository/txn/TransactionTest.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,120 +15,144 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import lv.nixx.poc.spring.data.JPAConfiguration;
 import lv.nixx.poc.spring.data.domain.txn.Currency;
 import lv.nixx.poc.spring.data.domain.txn.Transaction;
 import lv.nixx.poc.spring.data.repository.txn.CurrencyRepository;
 
+// TODO Make sample from there... https://docs.spring.io/spring-data/data-commons/docs/current/reference/html/#mapping.object-creation.details
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = JPAConfiguration.class)
 public class TransactionTest {
-	
+
 	@Autowired
 	private CurrencyRepository currencyRepo;
-	
+
 	@Autowired
 	private TransactionRepository txnRepo;
-	
+
+	private static final Currency USD = new Currency("USD", 810);
+	private static final Currency EUR = new Currency("EUR", 978);
+
+	private final LocalDateTime now = LocalDateTime.now();
+
 	@Before
 	public void init() {
 		txnRepo.deleteAll();
 		currencyRepo.deleteAll();
-		
-		currencyRepo.save(new Currency("USD", 810));
-		currencyRepo.save(new Currency("EUR", 978));
+
+		currencyRepo.save(USD);
+		currencyRepo.save(EUR);
 	}
-	
+
 	@Test
 	public void crudOperations() {
-		
-		final Currency usd = new Currency("USD", 810);
-		final Currency eur = new Currency("EUR", 978);
 
 		Iterable<Currency> allCurrencies = currencyRepo.findAll();
 		System.out.println(allCurrencies);
-		
-		LocalDateTime now = LocalDateTime.now();
-		
-		txnRepo.save(new Transaction(now, BigDecimal.valueOf(10.01), "descr1", usd, "acc1"));
-		Transaction txn2 = txnRepo.save(new Transaction(now, BigDecimal.valueOf(20.01), "descr2", eur, "acc2"));
-		
+
+		txnRepo.save(new Transaction(now, BigDecimal.valueOf(10.01), "descr1", USD, "acc1"));
+		Transaction txn2 = txnRepo.save(new Transaction(now, BigDecimal.valueOf(20.01), "descr2", EUR, "acc2"));
+
 		assertEquals(2, txnRepo.count());
-		
+
 		Transaction txn = txnRepo.findById(txn2.getId()).orElse(null);
 		assertEquals("descr2", txn.getDescription());
-		
+
 		assertFalse(txnRepo.existsById(-1L));
-		
+
 		txnRepo.deleteById(txn2.getId());
 		assertFalse(txnRepo.existsById(txn2.getId()));
-	
+
 	}
-	
+
 	@Test
 	public void requestsTest() {
-		// TODO Make sample from there... https://docs.spring.io/spring-data/data-commons/docs/current/reference/html/#mapping.object-creation.details
+		createInitialData();
 		
-		LocalDateTime now = LocalDateTime.now();
-
-		final Currency usd = new Currency("USD", 810);
-		final Currency eur = new Currency("EUR", 978);
-		
-		txnRepo.save(new Transaction(now, BigDecimal.valueOf(10.01), "descr1", usd, "account1"));
-		txnRepo.save(new Transaction(now, BigDecimal.valueOf(10.05), "descr2", eur, "account2"));
-		txnRepo.save(new Transaction(now, BigDecimal.valueOf(10.03), "descr3", usd, "account2"));
-		txnRepo.save(new Transaction(now, BigDecimal.valueOf(10.04), "descr4", usd, "account4"));
-
-		Collection<Transaction> txns = txnRepo.findAllByCurrency(usd);
+		Collection<Transaction> txns = txnRepo.findAllByCurrency(USD);
 		txns.forEach(System.out::println);
 		System.out.println("====================");
 
-		assertEquals(3, txns.size());
-		
+		assertEquals(5, txns.size());
+
 		Collection<Transaction> notExisting = txnRepo.findAllByCurrency(new Currency("RUB", 978));
 		System.out.println(notExisting);
-		
+
 		System.out.println("====================");
-		
+
 		txns = txnRepo.findAllByAccountOrderByAmount("account2");
 		txns.forEach(System.out::println);
-		
+
 		System.out.println("====================");
-		txns = txnRepo.findAllByCurrencyAlphaCode("EUR");
-		txns.forEach(t-> System.out.println("Txn by alphaCode: " + t));
+		txns = txnRepo.findAllByCurrencyAlphaCode(EUR.getAlphaCode());
+		
+		txns.forEach(t -> System.out.println("Txn by alphaCode: " + t));
 
 	}
 
-	
 	@Test
 	public void pagingSample() throws InterruptedException {
-		
-		final Currency usd = new Currency("USD", 810);
+
 		final int txnCount = 20;
-		final LocalDateTime now = LocalDateTime.now();
 
 		for (int i = 0; i < txnCount; i++) {
-			txnRepo.save(new Transaction(now, BigDecimal.valueOf(i), "descr" + i, usd, "acc1"));
+			txnRepo.save(new Transaction(now, BigDecimal.valueOf(i), "descr" + i, USD, "acc1"));
 		}
-		
+
 		assertEquals(20, txnRepo.count());
-		
+
 		Page<Transaction> page = null;
 		int p = 0;
 		do {
 			page = txnRepo.findAll(PageRequest.of(p, 8));
-			
+
 			List<Transaction> content = page.getContent();
 			System.out.println("Page: " + p + " count: " + page.getNumberOfElements() + " : " + content);
 			p++;
-			
+
 		} while (page.hasNext());
-	
+	}
+
+	@Test
+	public void findTopFirstSample() {
+		createInitialData();
 		
+		Collection<Transaction> result = txnRepo.findTop3ByOrderByAmountDesc();
+		result.forEach(System.out::println);
+		
+		assertEquals(3, result.size());
+		
+		System.out.println("==============================================");
+		result = txnRepo.findFirst3ByCurrencyAlphaCode(USD.getAlphaCode(), Sort.by("Amount").descending());
+		result.forEach(System.out::println);
 	}
 	
+	@Test
+	@Transactional(readOnly=true)
+	public void getAllStreamed() {
+		createInitialData();
+		
+		try (Stream<Transaction> all = txnRepo.streamAllPaged(Pageable.unpaged())) {
+			all.forEach(System.out::println);
+		}
+	
+	}
+
+	private void createInitialData() {
+		txnRepo.save(new Transaction(now, BigDecimal.valueOf(10.01), "descr1", USD, "account1"));
+		txnRepo.save(new Transaction(now, BigDecimal.valueOf(20.05), "descr2", EUR, "account2"));
+		txnRepo.save(new Transaction(now, BigDecimal.valueOf(10.03), "descr3", USD, "account2"));
+		txnRepo.save(new Transaction(now, BigDecimal.valueOf(77.77), "descr4", USD, "account4"));
+		txnRepo.save(new Transaction(now, BigDecimal.valueOf(4.2), "descr4", USD, "account4"));
+		txnRepo.save(new Transaction(now, BigDecimal.valueOf(1.0), "descr4", USD, "account4"));
+	}
 
 }
