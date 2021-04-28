1e10d8180b472a9a8c7359f5c0ee7a815b8b1aa3
nixx78 <nixx78@gmail.com>
2021-04-28 09:32:46 +0300

    Formula & Stypes for cells added

--- data-formats/src/test/java/lv/nixx/samples/excel/ExcelWriter.java
+++ data-formats/src/test/java/lv/nixx/samples/excel/ExcelWriter.java
@@ -1,10 +1,10 @@
 package lv.nixx.samples.excel;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileOutputStream;
@@ -14,29 +14,57 @@ public class ExcelWriter {
     @Test
     public void writeDataToExcelFile() throws Exception {
 
-        Workbook workbook = new HSSFWorkbook();
-
+        HSSFWorkbook workbook = new HSSFWorkbook();
         Sheet sheet = workbook.createSheet("Persons");
         sheet.setColumnWidth(0, 6000);
         sheet.setColumnWidth(1, 4000);
 
-        Row row1 = sheet.createRow(0);
-        row1.createCell(0).setCellValue("Name");
-        row1.createCell(1).setCellValue("Amount");
+        HSSFFont font= workbook.createFont();
+        font.setBold(true);
+        font.setFontName("Arial");
+
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setFont(font);
+
+        Row headersRow = sheet.createRow(0);
+
+        Cell cell0 = headersRow.createCell(0);
+        cell0.setCellValue("Name");
+        cell0.setCellStyle(cellStyle);
+
+        Cell cell1 = headersRow.createCell(1);
+        cell1.setCellValue("Amount");
+        cell1.setCellStyle(cellStyle);
+
+        Cell cell2 = headersRow.createCell(2);
+        cell2.setCellValue("Count");
+        cell2.setCellStyle(cellStyle);
+
+        Cell cell3 = headersRow.createCell(3);
+        cell3.setCellValue("Total");
+        cell3.setCellStyle(cellStyle);
 
         for (int i = 1; i <= 5; i++) {
             Row row = sheet.createRow(i);
             row.createCell(0).setCellValue("Name" + i);
             row.createCell(1).setCellValue(i * 100.0);
+            row.createCell(2).setCellValue(i);
+            int pos = i + 1;
+            row.createCell(3).setCellFormula("B" + pos + "+C" + pos);
         }
 
-        String excelFilePath = "./src/test/resources/created_table.xls";
-        FileOutputStream outputStream = new FileOutputStream(excelFilePath);
+        Row totalRow = sheet.createRow(6);
+        totalRow.setRowStyle(cellStyle);
+        Cell cell = totalRow.createCell(2);
+        cell.setCellStyle(cellStyle);
+        cell.setCellValue("Total");
+
+        totalRow.createCell(3).setCellFormula("SUM(D2:D6)");
+
+        FileOutputStream outputStream = new FileOutputStream("./src/test/resources/created_table.xls");
         workbook.write(outputStream);
         workbook.close();
-
-
-        workbook.close();
     }
+
 
 }
