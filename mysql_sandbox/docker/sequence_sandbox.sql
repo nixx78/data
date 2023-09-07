5b3438c6761cb3583b36acbddeea0552980b0aa4
nixx78 <nixx78@gmail.com>
2023-09-07 08:24:20 +0300

    Sample with sequences updates, sample cleanup

--- mysql_sandbox/docker/sequence_sandbox.sql
+++ mysql_sandbox/docker/sequence_sandbox.sql
@@ -1,14 +1,16 @@
 DROP TABLE IF EXISTS table_with_grouped_data;
 
-CREATE TABLE IF NOT EXISTS table_with_grouped_data(
+CREATE TABLE IF NOT EXISTS table_with_grouped_data (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    instrumentID integer NOT NULL,
+    instrumentID INTEGER NOT NULL,
     state ENUM('SUCCESS', 'FAIL'),
-    nvalue numeric(10,3),
-    date_time timestamp
+    nvalue NUMERIC(10 , 3 ),
+    date_time TIMESTAMP
 );
 
-DELETE FROM table_with_grouped_data WHERE id!=0;
+DELETE FROM table_with_grouped_data 
+WHERE
+    id != 0;
 
 INSERT INTO table_with_grouped_data (instrumentID, state, nvalue, date_time) VALUES
  (1, 'SUCCESS', 1.10, '2023-09-06 12:00:00'),
@@ -28,12 +30,11 @@ INSERT INTO table_with_grouped_data (instrumentID, state, nvalue, date_time) VAL
  (3, 'FAIL',	1.00, '2023-09-06 11:57:00'),
  (3, 'FAIL',	1.00,'2023-09-06 11:56:00');
 
- -- SELECT * FROM  table_with_grouped_data;
+SELECT * FROM  table_with_grouped_data;
 
- 
 -- Запрос расскрашиывает последовательности по группам для одного инстремента
 /*
-SELECT	*,  ROW_NUMBER() OVER (ORDER BY date_time) - ROW_NUMBER() OVER (PARTITION BY state ORDER BY date_time) AS seqId
+SELECT	*,  ROW_NUMBER() OVER (ORDER BY date_time desc) - ROW_NUMBER() OVER (PARTITION BY state ORDER BY date_time desc) AS seqId
 FROM table_with_grouped_data t
 WHERE t.instrumentID = 1
 ORDER BY  t.date_time desc;
@@ -43,49 +44,28 @@ ORDER BY  t.date_time desc;
 /*
 SELECT	*,
 ROW_NUMBER() OVER (PARTITION BY instrumentID, state ORDER BY date_time desc) AS seq,
-ROW_NUMBER() OVER (PARTITION BY instrumentID ORDER BY date_time) - ROW_NUMBER() OVER (PARTITION BY instrumentID, state ORDER BY date_time) AS seqId
+ROW_NUMBER() OVER (PARTITION BY instrumentID ORDER BY date_time desc) - ROW_NUMBER() OVER (PARTITION BY instrumentID, state ORDER BY date_time desc) AS seqId
 FROM table_with_grouped_data t
 ORDER BY  instrumentID, date_time desc;
 */
 
--- Надо получить длину последней последовательности для каждого инструментов
+-- Маркируем группы для всех инструментов
 /*
 SELECT	*,
 ROW_NUMBER() OVER (PARTITION BY instrumentID, state ORDER BY date_time desc) AS seq,
-ROW_NUMBER() OVER (PARTITION BY instrumentID ORDER BY date_time) - ROW_NUMBER() OVER (PARTITION BY instrumentID, state ORDER BY date_time) AS seqId
+ROW_NUMBER() OVER (PARTITION BY instrumentID ORDER BY date_time desc) - ROW_NUMBER() OVER (PARTITION BY instrumentID, state ORDER BY date_time desc) AS seqId
 FROM table_with_grouped_data t
 ORDER BY  instrumentID, date_time desc;
 */
 
 -- Получаем длину последовательности по state 
-/*
-SELECT * FROM  table_with_grouped_data;
-
 WITH RankedData AS (
 	SELECT	*,
 	ROW_NUMBER() OVER (PARTITION BY instrumentID ORDER BY date_time desc) - ROW_NUMBER() OVER (PARTITION BY instrumentID, state ORDER BY date_time desc) AS seqId
 	FROM table_with_grouped_data t
 	ORDER BY  instrumentID, date_time desc
 )
-SELECT instrumentID, count(*) as LastSequenceSize
-from RankedData
-where concat(instrumentID, "_", seqId) IN (
-	SELECT concat(instrumentID, "_", MAX(seqId)) as ID from RankedData
-	GROUP BY instrumentID
-    )
-GROUP BY instrumentID;
- */
-
--- Получаем длину последовательности по nvalue
-SELECT * FROM  table_with_grouped_data;
-
-WITH RankedData AS (
-	SELECT	*,
-	ROW_NUMBER() OVER (PARTITION BY instrumentID ORDER BY date_time desc) - ROW_NUMBER() OVER (PARTITION BY instrumentID, nvalue ORDER BY date_time desc) AS seqId
-	FROM table_with_grouped_data t
-	ORDER BY  instrumentID, date_time desc
-)
-SELECT instrumentID, count(*) as LastSequenceSize
+SELECT instrumentID, count(*) as LastSequenceSize_ByState
 from RankedData
 where concat(instrumentID, "_", seqId) IN (
 	SELECT concat(instrumentID, "_", MIN(seqId)) as ID from RankedData
@@ -93,3 +73,18 @@ where concat(instrumentID, "_", seqId) IN (
     )
 GROUP BY instrumentID;
  
+
+-- Получаем длину последовательности по nvalue
+WITH RankedData AS (
+	SELECT	*,
+	ROW_NUMBER() OVER (PARTITION BY instrumentID ORDER BY date_time desc) - ROW_NUMBER() OVER (PARTITION BY instrumentID, nvalue ORDER BY date_time desc) AS seqId
+	FROM table_with_grouped_data t
+	ORDER BY  instrumentID, date_time desc
+) 
+SELECT instrumentID, count(*) AS LastSequenceSize_ByValue
+FROM RankedData
+WHERE concat(instrumentID, "_", seqId) IN (
+	SELECT concat(instrumentID, "_", MIN(seqId)) AS ID FROM RankedData
+	GROUP BY instrumentID
+    )
+GROUP BY instrumentID;
