cc2daf06c49f01cb58bf06524da54aaba6fc41e2
nixx78 <nixx78@gmail.com>
2025-01-09 12:34:36 +0200

    Admin user created to play with MySQL settings on fly

--- spring-data-jpa/db/docker/sql/db_init.sql
+++ spring-data-jpa/db/docker/sql/db_init.sql
@@ -1,8 +1,11 @@
-SELECT '"app_user" user creation';
 CREATE USER IF NOT EXISTS 'app_user'@'%' IDENTIFIED BY 'app_pass';
-
-SELECT 'Set "app_user" permission';
 GRANT ALL PRIVILEGES ON db_sandbox.* TO 'app_user'@'%';
 
+SELECT '"app_user" created & permissions set';
+
+CREATE USER 'admin_user'@'%' IDENTIFIED BY 'admin_pass';
+GRANT ALL PRIVILEGES ON *.* TO 'admin_user'@'%' WITH GRANT OPTION;
+
+SELECT '"admin_user" created & permissions set';
 FLUSH PRIVILEGES;
-SELECT '"app_user" created';
+
